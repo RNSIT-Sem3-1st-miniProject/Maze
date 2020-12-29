56b7af3725362e3af4a5541ea5d85db336c8d35a
suprithsatish2001@gmail.com <suprithsatish2001@gmail.com>
2020-12-29 13:17:12 +0530

    suprith level changes

--- src/main.c
+++ src/main.c
@@ -11,25 +11,48 @@ int main(){
     setPlayer(&game, player);
     int levelNumber = 1;
     int gameResult = PLAY_NXT_GAME;
+    int choice = PLAY_NXT_GAME;
+    char ch;
     Level* level = NULL;
     while(
         (levelNumber <= MAX_LEVEL) &&
-        (gameResult != GAME_QUIT)
+        (choice != END_GAME)
     ){
         if (isDebugOn()){
             printf("While\n");
         }
         generateNxtLevel(&game);
         level = playLevel(&game);
-        if (level != NULL){
+        if (level != NULL)
+        {
             printMaze(level->maze);
             gameResult = movement(level->maze, level->maze->start);
-            if(gameResult == GAME_QUIT){
+            if(gameResult == GAME_QUIT)
+            {
+                reflectSolution(game.level->maze, game.level->solution);
+                printMaze(game.level->maze);
                 level->didQuitTheGame = yea_lmao;
             }
-            reflectSolution(game.level->maze, game.level->solution);
-            printMaze(game.level->maze);
-        }else{
+            if(levelNumber == MAX_LEVEL)
+            {   
+                printf("You have finished the game!\n");
+                disolveGame(&game);
+                return 1;
+            }
+            printf("Do you wish to continue(y or n)?");
+            ch = getchar();
+            getchar();
+            if(ch == 'y')
+            {
+                choice = PLAY_NXT_GAME;
+            }
+            else
+            {
+                choice = END_GAME;
+            }
+            printf("%d\n",choice);
+        }
+        else{
             disolveGame(&game);
             return 1;
         }
