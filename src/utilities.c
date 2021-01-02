ab83e6c0f8e2747f2f7f5fc3026cdaac26848b77
Vasu7777py <subbannavarvasu@gmail.com>
2021-01-02 19:53:05 +0530

    vasu for test running in windows!! ;>

--- src/utilities.c
+++ src/utilities.c
@@ -96,7 +96,7 @@ void reflectSolution(Maze* maze, LL* ll){
 
 void SETCOLOR(char preSetColor){
     Theme theme = getTheme(getSelectedThemeIndex());
-    if (COMPILE_MECHINE == WINDOWS){
+    if ((COMPILE_MECHINE == WINDOWS) and false){
         system("color 71");
     }else{
         if(preSetColor == WALL){
